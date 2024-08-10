1831bb1c074c72b755b3f9d251319679f1e2f1f2
Oleg K <tup1k@yandex.ru>
2024-08-10 20:36:49 +0300

    Добавлен тапбар

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Олег Кор on 07.08.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var signOutButton: UIButton!
+    @IBOutlet private var profileImage: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var accountDescription: UILabel!
+    @IBOutlet private var accountName: UILabel!
+    
+    @IBAction private func tapSignOutButton(_ sender: Any) {
+    }
+}
